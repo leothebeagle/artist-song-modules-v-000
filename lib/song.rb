dde3ff20423033bee81511d5f14a2ba9260b553a
Aziz Kosber <a.kosber@gmail.com>
2018-08-06 01:20:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,15 +4,15 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = [] #will collect all instances of songs created 
+  @@songs = [] #will collect all instances of songs created
 
   def initialize
     @@songs << self
   end
 
   def self.find_by_name(name) # allows you to call Song.find_by_name("weird fishes") #=> #<weird fishes instance> this instance should aready have its artist att set to radiohead.
-    #remember that its the responsibility of the Artist class to create a song and not Song. A song cant create itself, an artist can create it. 
-    @@songs.detect{|a| a.name == name}
+    #remember that its the responsibility of the Artist class to create a song and not Song. A song cant create itself, an artist can create it.
+    @@songs.detect{|a| a.name == name} #iterates over all the songs and returns the first one with that name.
   end
 
   def artist=(artist)
