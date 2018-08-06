31ea82eb4dfc500b7dbd4a7491db86763c89477e
Aziz Kosber <a.kosber@gmail.com>
2018-08-06 02:17:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,10 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name) # allows you to call Song.find_by_name("weird fishes") #=> #<weird fishes instance> this instance should aready have its artist att set to radiohead.
-    #remember that its the responsibility of the Artist class to create a song and not Song. A song cant create itself, an artist can create it.
-    @@songs.detect{|a| a.name == name} #iterates over all the songs and returns the first one with that name.
-  end
+  # def self.find_by_name(name) # allows you to call Song.find_by_name("weird fishes") #=> #<weird fishes instance> this instance should aready have its artist att set to radiohead.
+  #   #remember that its the responsibility of the Artist class to create a song and not Song. A song cant create itself, an artist can create it.
+  #   @@songs.detect{|a| a.name == name} #iterates over all the songs and returns the first one with that name.
+  # end
 
   def artist=(artist) #why are we specifiying it here and not relying on attr_writer or attr_accessor to write the method for us? isnt it doing the same thing?
     @artist = artist
